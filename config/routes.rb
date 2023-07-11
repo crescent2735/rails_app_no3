d99973a85d46698e3181f0b002102d3a04947cdd
渡邊美月 <watanabemizuki@watanabemidzukinoMacBook-Pro.local>
2023-07-11 15:32:46 +0900

    create User signup login logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'password_resets/new'
+  get 'password_resets/edit'
+  get 'sessions/new'
+  get '/signup',     to: 'users#new'
+  get    "/login",   to: "sessions#new"
+  post   "/login",   to: "sessions#create"
+  delete "/logout",  to: "sessions#destroy"
+  resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
