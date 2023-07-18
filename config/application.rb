b47afd4a4a0f82e2f36b07c6ca49db199e0ba862
渡邊美月 <watanabemizuki@watanabemidzukinoMacBook-Pro.local>
2023-07-18 21:49:22 +0900

    prduction14

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Myapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    onfig.middleware.delete ActionDispatch::HostAuthorization
+    config.middleware.delete ActionDispatch::HostAuthorization
 
     # Configuration for the application, engines, and railties goes here.
     #
